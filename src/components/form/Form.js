f92e145e967394f76d4c9af41dfbd4eaa4b301ee
Jai Parakh <jaiparakh.kota.10@gmail.com>
2020-09-15 00:00:11 +0530

    useless code removed

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -9,53 +9,13 @@ class Form extends Component{
     constructor(props){
         super(props);
         this.state = {
-            name: '',
-            email: '',
-            password: '',
-            passrep: '',
-            description: '',
-            linkedInUrl: '',
             data: ''
         }
     }
 
-
-    handleEmailChange = (e) => {
-        this.setState({email: e.target.value});
-    }
-
-    handleNameChange = (e) => {
-        this.setState({name: e.target.value});
-    }
-    
-    handlePasswordChange = (e) => {
-        this.setState({password: e.target.value});
-    }
-
-    handlePassRepChange = (e) => {
-        this.setState({passrep: e.target.value});
-    }
-
-    handleDescriptionChange = (e) => {
-        this.setState({description: e.target.value});
-    }
-
-    handleLinkedInChange = (e) => {
-        this.setState({linkedInUrl: e.target.value});
-    }
-
     submitData = (e) => {
         e.preventDefault();
-        //console.log(this.props.coords.latitude);
-        const user = {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password,
-            description: this.state.description,
-            linkedInUrl: this.state.linkedInUrl,
-        }
-        
-        //console.log(user);
+    
         axios.get("http://35.199.46.108/casRowSets/tables/cas~fs~cas-shared-default~fs~Public~fs~SCORE2_UNDEFINED_2020_09_12_11_15_40_OUTPUT/rows", {
             headers: {
                 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vbG9jYWxob3N0L1NBU0xvZ29uL3Rva2VuX2tleXMiLCJraWQiOiJsZWdhY3ktdG9rZW4ta2V5IiwidHlwIjoiSldUIn0.eyJqdGkiOiI1NzcyZTkwMzllYWM0MmNjODA0MTFlMzEzNmNiMDg5YiIsImV4dF9pZCI6ImNuPU1vZGVsIE1hbmFnZXIsY249VXNlcnMsZGM9c3RyYXRhY2VudGxhYnMsZGM9Y29tIiwic3ViIjoiZDM2ZDc0NWUtODQ0ZS00NzgwLTljM2EtZTk4NWQxNDU1YTJiIiwic2NvcGUiOlsib3BlbmlkIl0sImNsaWVudF9pZCI6ImFwcCIsImNpZCI6ImFwcCIsImF6cCI6ImFwcCIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfaWQiOiJkMzZkNzQ1ZS04NDRlLTQ3ODAtOWMzYS1lOTg1ZDE0NTVhMmIiLCJvcmlnaW4iOiJsZGFwIiwidXNlcl9uYW1lIjoic2FzbWRsbWdyIiwiZW1haWwiOiJzYXNtZGxtZ3JAdXNlci5mcm9tLmxkYXAuY2YiLCJhdXRoX3RpbWUiOjE2MDAxMDA4MjUsInJldl9zaWciOiJiZWFhOTY3OCIsImlhdCI6MTYwMDEwMDgyNSwiZXhwIjoxNjAwMTQ0MDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L1NBU0xvZ29uL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbImFwcCIsIm9wZW5pZCJdfQ.lQXsjsgUuZi_w4xL5za5MHPu_ycceTL3cPj0VxA4mvJUSj438eVPwtuOUfhFMG5xWtx1xb4b2wC--V0zfFQuFdef5wUMuebqJfb7RNzlo2MJ-_2MErC5QjpU4RgC2qaMLw3_q8oTms5qehbYAvQEMD1kbq23bE6crmftqND5uTjnHM1zMRi5CUUMdsHNpwvqjE26EVbaImh6k9n0tfSbKEGjDX9l2Eg6AeefCBmXpYJgwZvd_bi6mc--m5A70WOGDLxpTSRzdHAYhIZGVWfyvZ17irCePgDvMSN7PZotwEjOMmc_zjOtCO4lslVgRGA5iZOpfrswNYFHlphplFkaWg'
