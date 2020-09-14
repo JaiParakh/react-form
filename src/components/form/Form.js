88e431352ee8da7e8efff9a428721f8a63c91eb4
Jai Parakh <jaiparakh.kota.10@gmail.com>
2020-09-14 23:39:36 +0530

    form added

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -55,14 +55,14 @@ class Form extends Component{
         }
         
         //console.log(user);
-        axios.post("user/register", user).then(res => {
+        axios.get("http://35.199.46.108/casRowSets/tables/cas~fs~cas-shared-default~fs~Public~fs~SCORE2_UNDEFINED_2020_09_12_11_15_40_OUTPUT/rows", {
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vbG9jYWxob3N0L1NBU0xvZ29uL3Rva2VuX2tleXMiLCJraWQiOiJsZWdhY3ktdG9rZW4ta2V5IiwidHlwIjoiSldUIn0.eyJqdGkiOiI1NzcyZTkwMzllYWM0MmNjODA0MTFlMzEzNmNiMDg5YiIsImV4dF9pZCI6ImNuPU1vZGVsIE1hbmFnZXIsY249VXNlcnMsZGM9c3RyYXRhY2VudGxhYnMsZGM9Y29tIiwic3ViIjoiZDM2ZDc0NWUtODQ0ZS00NzgwLTljM2EtZTk4NWQxNDU1YTJiIiwic2NvcGUiOlsib3BlbmlkIl0sImNsaWVudF9pZCI6ImFwcCIsImNpZCI6ImFwcCIsImF6cCI6ImFwcCIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfaWQiOiJkMzZkNzQ1ZS04NDRlLTQ3ODAtOWMzYS1lOTg1ZDE0NTVhMmIiLCJvcmlnaW4iOiJsZGFwIiwidXNlcl9uYW1lIjoic2FzbWRsbWdyIiwiZW1haWwiOiJzYXNtZGxtZ3JAdXNlci5mcm9tLmxkYXAuY2YiLCJhdXRoX3RpbWUiOjE2MDAxMDA4MjUsInJldl9zaWciOiJiZWFhOTY3OCIsImlhdCI6MTYwMDEwMDgyNSwiZXhwIjoxNjAwMTQ0MDI0LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L1NBU0xvZ29uL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbImFwcCIsIm9wZW5pZCJdfQ.lQXsjsgUuZi_w4xL5za5MHPu_ycceTL3cPj0VxA4mvJUSj438eVPwtuOUfhFMG5xWtx1xb4b2wC--V0zfFQuFdef5wUMuebqJfb7RNzlo2MJ-_2MErC5QjpU4RgC2qaMLw3_q8oTms5qehbYAvQEMD1kbq23bE6crmftqND5uTjnHM1zMRi5CUUMdsHNpwvqjE26EVbaImh6k9n0tfSbKEGjDX9l2Eg6AeefCBmXpYJgwZvd_bi6mc--m5A70WOGDLxpTSRzdHAYhIZGVWfyvZ17irCePgDvMSN7PZotwEjOMmc_zjOtCO4lslVgRGA5iZOpfrswNYFHlphplFkaWg'
+            }
+        }).then(res => {
             //const {history} = this.props;
             console.log(res.data);
-            if(res.data.success){
-                //store.set('loggedIn', true);
-                //store.set('id', res.data.data._id);
-                //history.push('/maps');
-            }
+            
         });
     }
 
@@ -71,7 +71,10 @@ class Form extends Component{
             <div>
             
             <div className="register-photo">
-                <div className="form-container">
+                <form onSubmit={this.submitData}>
+                <input type="submit" className="btn btn-primary btn-block" value="Sign Up" />
+                </form>
+                {/*<div className="form-container">
                     <form onSubmit={this.submitData}>
                         <h2 className="text-center"><strong>Create</strong> an account.</h2>
                         <div className="form-group">
@@ -104,7 +107,7 @@ class Form extends Component{
                             <input type="submit" className="btn btn-primary btn-block" value="Sign Up" />
                         </div>
                     </form>
-                </div>
+                </div>*/}
             </div>
             </div>
         );
